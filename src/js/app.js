3742a05efe8df485e051e5ee1aace5b1a47da713
max <maks@coderiver.com.ua>
2019-01-28 09:44:44 +0200

    sidebar

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,19 @@
 const btn = $('.js-fixed-menu-btn');
-const hiddenContent = $('.js-fixed-menu-content');
+const burgerBtn = $('.js-burger');
+const app = $('.js-app');
+const sidebar = $('.js-sidebar');
 
 btn.on('click', (e)=> {
   $(e.currentTarget).parents('.js-fixed-menu').toggleClass('is-active');
-  // hiddenContent.toggleClass('is-visible');
+});
+
+burgerBtn.on('click', () => {
+  app.toggleClass('menu-open');
+});
+
+app.mouseup(function(e) {
+  const div = sidebar;
+  if (div.has(e.target).length === 0 && app.hasClass('menu-open')) {
+    app.removeClass('menu-open');
+  }
 });
