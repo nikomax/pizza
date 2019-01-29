7917fcc928888c39e0eb6c2a784ffd876cb2faf8
max <maks@coderiver.com.ua>
2019-01-29 19:42:49 +0200

    code page

--- src/js/app.js
+++ src/js/app.js
@@ -2,8 +2,9 @@ const btn = $('.js-fixed-menu-btn');
 const burgerBtn = $('.js-burger');
 const app = $('.js-app');
 const sidebar = $('.js-sidebar');
+const verification = $('.js-verification');
 
-btn.on('click', (e)=> {
+btn.on('click', (e) => {
   $(e.currentTarget).parents('.js-fixed-menu').toggleClass('is-active');
 });
 
@@ -11,9 +12,50 @@ burgerBtn.on('click', () => {
   app.toggleClass('menu-open');
 });
 
-app.mouseup(function(e) {
+app.mouseup(function (e) {
   const div = sidebar;
   if (div.has(e.target).length === 0 && app.hasClass('menu-open')) {
     app.removeClass('menu-open');
   }
 });
+
+
+function goToNextInput(e) {
+  var key = e.which,
+    t = $(e.target),
+    sib = t.next('input');
+
+  if (key !== 9 && (key < 48 || key > 57)) {
+    e.preventDefault();
+    return false;
+  }
+
+  if (key === 9) {
+    return true;
+  }
+
+  if (!sib || !sib.length) {
+    sib = verification.find('input').eq(0);
+  }
+  sib.select().focus();
+}
+
+function onKeyDown(e) {
+  var key = e.which;
+
+  if (key === 9 || (key >= 48 && key <= 57)) {
+    return true;
+  }
+
+  e.preventDefault();
+  return false;
+}
+
+function onFocus(e) {
+  $(e.target).select();
+}
+
+verification.on('keyup', 'input', goToNextInput);
+verification.on('keydown', 'input', onKeyDown);
+verification.on('click', 'input', onFocus);
+
