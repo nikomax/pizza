edd3ed12a182bf69235a283a620aab2582780c94
max <maks@coderiver.com.ua>
2019-02-05 11:10:56 +0200

    init loyalty bank

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -8,3 +8,25 @@ $('.js-slider').slick({
   rows: 0,
   dots: true
 });
+
+$('.js-loyalty-slider').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  rows: 0,
+  dots: false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+
+$('.js-menu-slider').slick({
+  infinite: true,
+  slidesToShow: 5,
+  centerPadding: '0',
+  centerMode: true,
+  slidesToScroll: 1,
+  arrows: false,
+  dots: false,
+  focusOnSelect: true
+});
